5b473f5e830f79ada1f1c80a0b3eee1dc68757ee
Arun Jois <arun.joiss@gmail.com>
2019-12-17 19:12:24 +0530

    Hospital Working

--- Hospital.java
+++ Hospital.java
@@ -1,35 +1,65 @@
 import java.util.Scanner;
+
 public class Hospital {
+	public static void print(int capacity[][],int n) {
+		System.out.println("*********************************");
+		System.out.println("Printing the matrix After Deduction");
+		for (int p = 0; p < n; p++) {
+			for (int q = 0; q < 2; q++)
+				System.out.print(capacity[p][q] + "\t");
+			System.out.println();
+		}
+		System.out.println("*********************************");
+	}
+
 	public static void main(String args[]) {
-		int capacity[][] = new int[3][2];
-		System.out.println("Enter Matrix");
+		int n;
 		Scanner sc = new Scanner(System.in);
-		for (int i = 0; i < 3; i++)
+		System.out.println("Enter the value of n");
+		n = sc.nextInt();
+		int capacity[][] = new int[n][2];
+		System.out.println("Enter Matrix");
+		for (int i = 0; i < n; i++)
 			for (int j = 0; j < 2; j++)
 				capacity[i][j] = sc.nextInt();
-		while(true) {
-			System.out.println("Enter the Status of Paitent(C/N):" );
+		while (true) {
+			System.out.println("Enter C For Admit of Critical Paitent");
+			System.out.println("Enter N For Admit of Non-Cricitcal Paitent");
 			System.out.println("Enter P for priting of matrix");
+			System.out.println("Enter E for Exit");
 			char ch = sc.next().charAt(0);
-			switch(ch) {
-				case 'c':
-				case 'C' :capacity[0][0]=capacity[0][0]-1;
+			switch (ch) {
+			case 'c':
+			case 'C':
+				System.out.println("Enter the Hospital Number");
+				int i = sc.nextInt();
+				if (i != 0)
+					i = i - 1;
+				capacity[i][0] = capacity[i][0] - 1;
+				print(capacity,n);
 				break;
-				case 'n':
-				case 'N' :capacity[0][1]=capacity[0][1]-1;
+			case 'n':
+			case 'N':
+				System.out.println("Enter the Hospital Number");
+				int j = sc.nextInt();
+				if (j != 0)
+					j = j - 1;
+				capacity[j][1] = capacity[j][1] - 1;
+				print(capacity,n);
 				break;
-				case 'p':
-				case 'P': 
-				for(int i=0;i<3;i++) {
-					for(int j=0;j<2;j++) 
-						System.out.print(capacity[i][j]+"\t");	
+			case 'p':
+			case 'P':
+				for (int p = 0; p < n; p++) {
+					for (int q = 0; q < 2; q++)
+						System.out.print(capacity[p][q] + "\t");
 					System.out.println();
 				}
+				print(capacity,n);
 				break;
-				case 'e':
-				case 'E':
+			case 'e':
+			case 'E':
 				System.exit(0);
 			}
-		}		
+		}
 	}
 }
